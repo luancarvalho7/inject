b0b9643d01f3c95c7c6cdd96b085c65ddb657957
Luan Oak <99412966+luancarvalho7@users.noreply.github.com>
2023-10-18 12:04:15 -0300

    Add files via upload

--- src/assets/components/stats/percentage.jsx
+++ src/assets/components/stats/percentage.jsx
@@ -1,7 +1,18 @@
-export function Percentage(){
-    return <>
-    <div className="statsPercentage">
-        <h1>+35%</h1>
-    </div>
-    </>
-}
+export function Percentage({ value }) {
+    let className = "statsPercentage";
+  
+    if (value > 10) {
+      className += " sp-good";
+    } else if (value > 0 && value <= 10) {
+      className += " sp-medium";
+    } else {
+      className += " sp-bad";
+    }
+  
+    return (
+      <div className={className}>
+        <h1>{value > 0 ? `+${value}%` : `${value}%`}</h1>
+      </div>
+    );
+  }
+  
--- src/assets/components/stats/stats.jsx
+++ src/assets/components/stats/stats.jsx
@@ -1,12 +1,56 @@
 import './stats.css'
 import { Percentage } from './percentage.jsx'
 
+let lastRandomValue = null;
+
+function getRandomValue() {
+  const today = new Date();
+  const day = today.getDate();
+  const currentHour = today.getHours();
+
+  // Define time intervals (e.g., morning, afternoon, evening)
+  let timeInterval;
+  if (currentHour < 8) {
+    timeInterval = 'morning';
+  } else if (currentHour < 16) {
+    timeInterval = 'afternoon';
+  } else {
+    timeInterval = 'evening';
+  }
+
+  // Generate a seed based on the day and the current time interval
+  const rng = seedrandom(`${day}-${timeInterval}`);
+
+  // Generate the random value based on the seed
+  let randomValue = rng() * (32 - (-15)) + (-15); // Random value between -15 and +32
+  
+  // If it's the first random value of the day, save it
+  if (lastRandomValue === null) {
+    lastRandomValue = randomValue;
+  } else {
+    // Otherwise, limit the fluctuation based on the first random value of the day
+    const minLimit = lastRandomValue - 5; // 5% less than the first value
+    const maxLimit = lastRandomValue + 5; // 5% more than the first value
+    
+    randomValue = Math.min(Math.max(randomValue, minLimit), maxLimit);
+  }
+
+  return randomValue;
+}
+
+// Reset `lastRandomValue` at the start of each new day
+// This could be done using a scheduled job or other mechanism
+function resetLastRandomValue() {
+  lastRandomValue = null;
+}
+
+
 export function Stats(){
     return <>
     <div className="statsInfo">
-        <h2>Pagamento</h2>
-        <h1>R$ 400K</h1>
-        <Percentage/>
+        <h2 className="secondText statsTitle">Pagamento</h2>
+        <h1 className="mainText statsValue">R$ 400K</h1>
+        <Percentage value="10"/>
     </div>
 </>
 }
