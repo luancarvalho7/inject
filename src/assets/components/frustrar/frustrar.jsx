1cff4b6a34d8eadcc9f7edeca854cebd4209302d
Amin Morais <111761627+dev1amin@users.noreply.github.com>
2023-10-19 12:50:03 +0000

    feat: Estilização frustrar conta

--- src/assets/components/frustrar/frustrar.jsx
+++ src/assets/components/frustrar/frustrar.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import './frustrar.css'
+import arrow from '../../images/chevron-right.svg'
+import logo from '../../images/logo.png'
 
 import { Stats } from "../stats/stats";
 
@@ -16,6 +18,10 @@ export function MinesGame() {
 
     return (
         <div className="container">
+            <div className="vip-container">
+                <img src={logo}/>
+                <button className="vip-button">Acesso VIP <img src={arrow}/></button>
+            </div>
             <div className="box">
                 <h1 className="game-title">Mines</h1>
                 <div className="progress-bar">
@@ -27,12 +33,14 @@ export function MinesGame() {
                 <div className="game-info-container">
                     <p className="game-info">O sistema da SPRIBE irá te identificar como um jogador “frustrado” da BullsBet e vai fazer com que suas chances de ganhar aumentem</p>
                 
-                    <Stats title={'Pagamento'} value={'R$20'}/>
-                <Stats title={'Faturamento'} value={'R$100'}/>
+                    <div className="game-pay-reve">
+                        <Stats title={'Faturamento'} value={'R$100'}/>
+                        <Stats title={'Pagamento'} value={'R$20'}/>
+                    </div>
                 </div>
                 <button className="frustrate-button">Frustrar Conta</button>
             </div>
-            <iframe src="https://bullsbet.net/" width="80%" height="1100px" frameBorder="0"></iframe>
+            <iframe src="https://bullsbet.net/" width="100%" height="1100px" frameBorder="0"></iframe>
         </div>
     );
 };
