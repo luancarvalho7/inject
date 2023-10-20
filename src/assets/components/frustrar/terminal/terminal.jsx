82c9eeb2f173b33d9e2b9f4698ee025802ba7784
Luan Carvalho <luancarvprofissional@gmail.com>
2023-10-20 01:50:27 -0300

    xd

--- src/assets/components/frustrar/terminal/terminal.jsx
+++ src/assets/components/frustrar/terminal/terminal.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './terminal.css';
 
-export function Terminal({house, supplier, game }) {
+export function Terminal({ house, supplier, game }) {
 
   function formatString(inputString) {
     return inputString.replace(/\s+/g, '_').toLowerCase();
@@ -54,21 +54,28 @@ export function Terminal({house, supplier, game }) {
   }, []);
 
   return (
-    <div id="terminal">
+    <div id="terminalSection">
+      <div className="t-labels-holder">
+        <p className='tLabel secondText'> {`${house} > ${supplier} > ${game}`} </p>
+        <p className='tLabel secondText'> Terminal </p>
+      </div>
 
-      {currentTask >= 1 && <p><span className='t-grey'>Iniciando o processo de</span> Frustração <span className='t-grey'> via </span> <span className='t-orange'> Inject</span></p>}
-      {currentTask >= 2 && <p>Conectando à<span className='t-yellow'> API </span></p>}
-      {currentTask >= 3 && <p>Conectando à <span className='t-green'>{fHouse}.</span><span className='t-pink'>{fGame}</span></p>}
-      {currentTask >= 4 && <p>
-        Baixando configuração{' '}
-        <span className="t-grey">
+
+      <div id="terminal">
+        {currentTask >= 1 && <p><span className='t-grey'>Iniciando o processo de</span> Frustração <span className='t-grey'> via </span> <span className='t-orange'> Inject</span></p>}
+        {currentTask >= 2 && <p>Conectando à<span className='t-yellow'> API </span></p>}
+        {currentTask >= 3 && <p>Conectando à <span className='t-green'>{fHouse}.</span><span className='t-pink'>{fGame}</span></p>}
+        {currentTask >= 4 && <p>
+          Baixando configuração{' '}
+          <span className="t-grey">
           // [{'.'.repeat(loadingPercentage / 10)}] {loadingPercentage}%
-        </span>
-      </p>}
-      {currentTask >= 5 && <p>Criando <span className='t-red'>requisição</span><span className='t-orange'> Inject </span></p>}
-      {currentTask >= 6 && <p>Enviando <span className='t-red'>requisição</span> à <span className='t-yellow'>api</span><span className='t-green'>.{fSupplier}.co/</span><span className='t-pink'>aviator</span></p>}
-      {currentTask >= 7 && <p>Alternado <span className='t-white'>status</span> da conta</p>}
-      {currentTask >= 8 && <p><span className='t-grey'>//// <br /> Sucesso!</span></p>}
+          </span>
+        </p>}
+        {currentTask >= 5 && <p>Criando <span className='t-red'>requisição</span><span className='t-orange'> Inject </span></p>}
+        {currentTask >= 6 && <p>Enviando <span className='t-red'>requisição</span> à <span className='t-yellow'>api</span><span className='t-green'>.{fSupplier}.co/</span><span className='t-pink'>aviator</span></p>}
+        {currentTask >= 7 && <p>Alternado <span className='t-white'>status</span> da conta</p>}
+        {currentTask >= 8 && <p><span className='t-grey'>//// <br /> Sucesso!</span></p>}
+      </div>
     </div>
   );
 }
