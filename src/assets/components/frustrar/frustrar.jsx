a02ecd1a6a1ec326a844ce7604bf4d18cbec389c
Amin Morais <111761627+dev1amin@users.noreply.github.com>
2023-10-21 04:16:13 +0000

    click frustrar

--- src/assets/components/frustrar/frustrar.jsx
+++ src/assets/components/frustrar/frustrar.jsx
@@ -5,14 +5,10 @@ import logo from '../../images/logo.png'
 
 import { Terminal } from "./terminal/terminal";
 import { useLocation, useNavigate } from 'react-router-dom';
-
-
 import { Stats } from "../stats/stats";
 
 export function Frustrar({ data }) {
-
     const navigate = useNavigate();
-
 
     function formatBigNumber(number) {
         if (number >= 1e6) {
@@ -24,39 +20,27 @@ export function Frustrar({ data }) {
         }
     }
 
-
     useEffect(() => {
-
         if (typeof (data) == 'string') {
             navigate('/')
         }
-
         if (data == null) {
             navigate('/')
         }
-
         if (data !== 'string' && data !== null) {
-
             const gamePercentage = (data.revenuePercentage * 3).toFixed(0);
             setValorVariavel(gamePercentage);
             setGameRev(formatBigNumber(data.revenue))
             setGamePay(formatBigNumber(data.revenue * 0.2))
-
         }
     }, []);
 
-
-    const [gameRev, setGameRev] = useState('formatBigNumber(data.revenue)')
-    const [gamePay, setGamePay] = useState('formatBigNumber(data.revenue * 0.2)')
-
-
-
-    // codigo pra aumentar a % 
+    const [gameRev, setGameRev] = useState('formatBigNumber(data.revenue)');
+    const [gamePay, setGamePay] = useState('formatBigNumber(data.revenue * 0.2)');
 
     const [valorVariavel, setValorVariavel] = useState(50);
     const progressBarRef = useRef(null);
     const percentageValueRef = useRef(null);
-
 
     useEffect(() => {
         const largura = `${valorVariavel * 1.5}%`;
@@ -64,48 +48,50 @@ export function Frustrar({ data }) {
         percentageValueRef.current.textContent = `${valorVariavel}%`;
     }, [valorVariavel]);
 
+    const [frustrou, setFrustrou] = useState(false);
 
-    const [frustrou,setFrustrou] = useState(false)
-
-    function runFrustrar(){
-        console.log('frustrado ok')
+    function runFrustrar() {
+        console.log('frustrado ok');
+        setShowTerminal(true);
     }
 
+    const [showTerminal, setShowTerminal] = useState(false);
 
     return (
         <div className="container">
             <div className="vip-container">
-                <img src={logo} />
-                <button className="vip-button">Acesso VIP <img src={arrow} /></button>
+                <img src={logo} alt="Logo" />
+                <button className="vip-button">Acesso VIP <img src={arrow} alt="Seta" /></button>
             </div>
             <div className="box">
-                <h1 className="game-title">Mines</h1>
-                <div className="progress-bar">
-                    <div className="percentage-indicator" id="percentageIndicator">
-                        <span ref={percentageValueRef} id="percentageValue"></span>
+                <div className={`box-content ${showTerminal ? "blur" : ""}`}>
+                    <h1 className="game-title">Mines</h1>
+                    <div className="progress-bar">
+                        <div className="percentage-indicator" id="percentageIndicator">
+                            <span ref={percentageValueRef} id="percentageValue"></span>
+                        </div>
+                        <div ref={progressBarRef} className="progress-fill" id="progressFill"></div>
                     </div>
-                    <div ref={progressBarRef} className="progress-fill" id="progressFill"></div>
-                </div>
-                <div className="game-info-container">
-                    <p className="game-info">O sistema da SPRIBE irá te identificar como um jogador “frustrado” da BullsBet e vai fazer com que suas chances de ganhar aumentem</p>
-
-                    <div className="game-pay-reve">
-                        <Stats
-                            title={'Pagamento'}
-                            value={`R$ ${gamePay}`}
-                        />
-                        <Stats
-                            title={'Faturamento'}
-                            value={`R$ ${gameRev}`}
-                        />
+                    <div className="game-info-container">
+                        <p className="game-info">O sistema da SPRIBE irá te identificar como um jogador “frustrado” da BullsBet e vai fazer com que suas chances de ganhar aumentem</p>
+                        <div className="game-pay-reve">
+                            <Stats
+                                title={'Pagamento'}
+                                value={`R$ ${gamePay}`}
+                            />
+                            <Stats
+                                title={'Faturamento'}
+                                value={`R$ ${gameRev}`}
+                            />
+                        </div>
                     </div>
+                    <button className="frustrate-button" onClick={runFrustrar}>Frustrar Conta</button>
                 </div>
-                <button className="frustrate-button" onClick={runFrustrar}>Frustrar Conta</button>
+                {showTerminal && (
+                    <Terminal house={'bullsbet'} supplier={'xdd'} game='aviator' />
+                )}
             </div>
-
-            <Terminal house={'bullsbet'} supplier={'xdd'} game='aviator'/>
             <iframe src="https://bullsbet.net/" width="100%" height="620px" frameBorder="0"></iframe>
         </div>
     );
-};
-
+}
