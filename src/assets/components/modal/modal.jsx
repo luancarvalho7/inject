81cb528a874073f23a59463d46bd6965504eb399
Amin Morais <111761627+dev1amin@users.noreply.github.com>
2023-10-27 11:07:17 +0000

    modal base construção

--- src/assets/components/modal/modal.jsx
+++ src/assets/components/modal/modal.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import './modal.css';
 
-function Modal({ isOpen, onClose, title, content }) {
+import diamond from '../../images/diamond.png'
+
+function Modal({ isOpen, onClose }) {
     if (!isOpen) return null;
 
     return (
         <div className="modal-background">
             <div className="modal-content">
                 <div className="modal-header">
-                    <h2>{title}</h2>
+                    <img src={diamond} alt="Acesso VIP" />
+                    <h1><strong>Acesso VIP</strong></h1>
                     <button onClick={onClose} className="close-button">X</button>
                 </div>
                 <div className="modal-body">
-                    {content}
+                    <p>Monitore todas as casas, tenha acesso ao sistema de todos os fornecedores e veja os melhores jogos.</p>
+                </div>
+                <div className="modal-footer">
+                    <button className="vip-button"><strong>QUERO SER VIP</strong></button>
+                    <button className="close-button" onClick={onClose}>Não, obrigado</button>
                 </div>
             </div>
         </div>
